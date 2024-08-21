f562ffa6be4b4ecc0d67a93a9b3709906b0e0d0e
duyplus <duyplusdz@gmail.com>
2024-08-22 00:22:57 +0700

    update files

--- src/main/java/com/codedrop/service/impl/UserDetailsImpl.java
+++ src/main/java/com/codedrop/service/impl/UserDetailsImpl.java
@@ -4,6 +4,7 @@ import com.codedrop.dto.UserDto;
 import com.codedrop.model.User;
 import com.codedrop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,6 +20,7 @@ public class UserDetailsImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Autowired
+    @Lazy
     private PasswordEncoder pe;
 
     @Override
